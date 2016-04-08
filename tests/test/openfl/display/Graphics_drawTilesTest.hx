bfbe5f3adb5cfaae817faab8b6090983d49aa74b
damrem <damien.remars@gmail.com>
2016-04-08 09:42:23 +0200

    test drawTiles with simple transform

--- tests/test/openfl/display/Graphics_drawTilesTest.hx
+++ tests/test/openfl/display/Graphics_drawTilesTest.hx
@@ -150,7 +150,35 @@ class Graphics_drawTilesTest
 		Assert.areEqual(32, b.height);
 	}
 	
+	@Test 
+	public function useTransform()
+	{
+		var ts = new Tilesheet(img);
+		ts.addTileRect(new Rectangle(0, 0, 32, 32));
+		holder.graphics.drawTiles(ts, [0, 0, 0, 1, 0, 0, 1], false, Tilesheet.TILE_TRANS_2x2);
+		
+		var b = holder.getBounds(context);
+		trace(b);
+		Assert.areEqual(0, b.x);
+		Assert.areEqual(0, b.y);
+		Assert.areEqual(32, b.width);
+		Assert.areEqual(32, b.height);
+	}
 	
+	@Test 
+	public function useTransformWithTileCenter()
+	{
+		var ts = new Tilesheet(img);
+		ts.addTileRect(new Rectangle(0, 0, 32, 32), new Point(16,16));
+		holder.graphics.drawTiles(ts, [0, 0, 0, 1, 0, 0, 1], false, Tilesheet.TILE_TRANS_2x2);
+		
+		var b = holder.getBounds(context);
+		trace(b);
+		Assert.areEqual(-16, b.x);
+		Assert.areEqual(-16, b.y);
+		Assert.areEqual(32, b.width);
+		Assert.areEqual(32, b.height);
+	}
 	
 	
 	
